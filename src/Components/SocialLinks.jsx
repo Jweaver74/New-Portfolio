719254b71b079535846d8e0b9565971439f01587
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-06-06 11:24:09 -0500

    added file and folders

---
+++ src/Components/SocialLinks.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icons } from "../Constants/constants";
+
+const SocialLinks = () => {
+  return (
+    <div className="w-full h-[100px] bg-grayscale-950 flex justify-center items-center gap-3 p-6">
+      <a href="https://github.com/Jweaver74" target="_blank">
+        <FontAwesomeIcon
+          className="text-3xl text-grayscale-50 hover:text-primary-400 hover:scale-[1.1] transition-all duration-75"
+          icon={icons.faGithub}
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/jasonweaver2/" target="_blank">
+        <FontAwesomeIcon
+          className="text-3xl text-grayscale-50 hover:text-primary-400 hover:scale-[1.1] transition-all duration-75"
+          icon={icons.faYoutube}
+        />
+      </a>
+    </div>
+  );
+};
+
+export default SocialLinks;
